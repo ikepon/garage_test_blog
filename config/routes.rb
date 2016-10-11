6ae2ad46a28397f1d425d064cad276743e1e6f45
ikepon <tatsuyanoheya@gmail.com>
2016-10-11 14:33:40 +0900

    root_path を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # doorkeeper で root_path が必要なので仮で設定
+  root :to => 'users#root'
+
   use_doorkeeper
 
   scope :v1 do

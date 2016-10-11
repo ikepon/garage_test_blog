6fedd63b6bb2ac47c3b2515b67b265581ccba85b
ikepon <tatsuyanoheya@gmail.com>
2016-10-11 15:34:47 +0900

    UserController#show, update の追加

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,4 +11,24 @@ RSpec.describe 'users', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'GET /v1/users/:user_id' do
+    let!(:user) { create(:user, name: 'alice') }
+
+    it 'returns user resource' do
+      get "/v1/users/#{user.id}", params, env
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'PUT /v1/users/:user_id' do
+    let!(:user) { create(:user, name: 'alice') }
+
+    before { params[:name] = 'bob' }
+
+    it 'updates user resource' do
+      put "/v1/users/#{user.id}", params, env
+      expect(response).to have_http_status(204)
+    end
+  end
 end

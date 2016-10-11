1b71f7a912d2fdd235fe244309f97c078e904e8a
ikepon <tatsuyanoheya@gmail.com>
2016-10-11 14:26:14 +0900

    Garage を使ってモデルとリソースを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
+  include Garage::Representer # リソースのエンコーディング・シリアライゼーションを提供するモジュール
+  include Garage::Authorizable # アクセスコントロール機能を提供するモジュール
+
+  # property でリソースが持つ属性を宣言
+  property :id
+  property :name
+  property :email
+
+  # その他、link を用いて他のリソースへのリンクを宣言することも可
+
+  def self.build_permissions(perms, other, target)
+    perms.permits! :read
+  end
+
+  def build_permissions(perms, other)
+    perms.permits! :read
+    perms.permits! :write
+  end
 end
